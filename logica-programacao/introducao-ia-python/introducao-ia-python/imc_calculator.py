e7760897e789d597fd1faee3446def0685fcd24d
Yuri Bastos <yuribbastos11@gmail.com>
2026-03-09 16:58:27 -0300

    Update imc_calculator.py

--- logica-programacao/introducao-ia-python/introducao-ia-python/imc_calculator.py
+++ logica-programacao/introducao-ia-python/introducao-ia-python/imc_calculator.py
@@ -1,1 +1,21 @@
+# Calculadora de IMC
 
+# pedir dados ao usuário
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite sua altura (m): "))
+
+# calcular IMC
+imc = peso / (altura ** 2)
+
+# mostrar resultado
+print("Seu IMC é:", round(imc, 2))
+
+# classificação
+if imc < 18.5:
+    print("Classificação: Abaixo do peso")
+elif imc < 25:
+    print("Classificação: Peso normal")
+elif imc < 30:
+    print("Classificação: Sobrepeso")
+else:
+    print("Classificação: Obesidade")
